55dd37c9db3a5081e52b57e5f1d5e1578cdffead
Alan Wang <44191076+alankrantas@users.noreply.github.com>
2019-05-16 22:31:14 +0800

    更新。

--- README.md
+++ README.md
@@ -1,24 +1,19 @@
-# pxt-esp8266_thingspeak
+# MakeCode Extension for ESP8266 Wifi Modules and ThingSpeak
 
+This extension/package is a modified version from [elecfreaks/pxt-esp8266iot](https://github.com/elecfreaks/pxt-esp8266iot).
 
+You need a account and a channel on [ThingSpeak](https://thingspeak.com/) to get the write API key.
 
-## TODO
+![pctdetail 775-090 1](https://user-images.githubusercontent.com/44191076/50425186-76ada780-08ac-11e9-956c-9ebd6be09bb2.jpg)
+![esp8266-pinout](https://user-images.githubusercontent.com/44191076/50428909-fc097a00-08f5-11e9-91f1-921d1b957f29.png)
+![esp8266_bb](https://user-images.githubusercontent.com/44191076/50541952-e513a200-0beb-11e9-9820-05f6798b2044.png)
 
-- [ ] Add a reference for your blocks here
-- [ ] Add "icon.png" image (300x200) in the root folder
-- [ ] Add "- beta" to the GitHub project description if you are still iterating it.
-- [ ] Turn on your automated build on https://travis-ci.org
-- [ ] Use "pxt bump" to create a tagged release on GitHub
-- [ ] Get your package reviewed and approved https://makecode.microbit.org/packages/approval
-
-Read more at https://makecode.microbit.org/packages/build-your-own
+Connect VCC and CH to 3.3V (sufficint power needed; the power from micro:bit's USB cable is NOT ENOUGH), GND to GND, RX and TX to two I/O pins, ignore the rest. See [here](https://components101.com/wireless/esp8266-pinout-configuration-features-datasheet) for more details.
 
 ## License
 
-
+MIT
 
 ## Supported targets
 
 * for PXT/microbit
-(The metadata above is needed for package search.)
-
--- main.ts
+++ main.ts
@@ -1,7 +1,95 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * MakeCode extension for ESP8266 Wifi modules and ThinkSpeak website https://thingspeak.com/
+ */
+//% color=#009b5b icon="\uf1eb" block="ESP8266 ThingSpeak"
+namespace ESP8266_ThingSpeak {
+
+    let wifi_connected: boolean = false
+    let thingspeak_connected: boolean = false
+    let last_upload_successful: boolean = false
+
+    // write AT command with CR+LF ending
+    function sendAT(command: string, wait: number = 100) {
+        serial.writeString(command + "\u000D\u000A")
+        basic.pause(wait)
+    }
+
+    // wait for certain response from ESP8266
+    function wait_for_response(): boolean {
+        let serial_str: string = ""
+        let result: boolean = false
+        let time: number = input.runningTime()
+        while (true) {
+            serial_str += serial.readString()
+            if (serial_str.length > 200) serial_str = serial_str.substr(serial_str.length - 200)
+            if (serial_str.includes("OK") || serial_str.includes("ALREADY CONNECTED")) {
+                result = true
+                break
+            } else if (serial_str.includes("ERROR") || serial_str.includes("SEND FAIL")) {
+                break
+            }
+            if (input.runningTime() - time > 300000) break
+        }
+        return result
+    }
+
+    //% block="Initialize ESP8266|TX %tx|RX %rx|baud rate %baudrate|Wifi SSID = %ssid|Wifi PW = %pw"
+    //% tx.defl=SerialPin.P0
+    //% rx.defl=SerialPin.P1
+    export function initialize_wifi(tx: SerialPin, rx: SerialPin, baudrate: BaudRate, ssid: string, pw: string) {
+        wifi_connected = false
+        thingspeak_connected = false
+        serial.redirect(
+            tx,
+            rx,
+            baudrate
+        )
+        sendAT("AT+RESTORE", 1000)
+        sendAT("AT+CWMODE=1")
+        sendAT("AT+RST", 1000)
+        sendAT("AT+CWJAP=\"" + ssid + "\",\"" + pw + "\"", 0)
+        wifi_connected = wait_for_response()
+        basic.pause(100)
+    }
+
+    //% block="Upload data to ThingSpeak|URL/IP = %ip|Write API key = %write_api_key|Field 1 = %n1|Field 2 = %n2|Field 3 = %n3|Field 4 = %n4|Field 5 = %n5|Field 6 = %n6|Field 7 = %n7|Field 8 = %n8"
+    //% ip.defl=api.thingspeak.com
+    export function connect_thingspeak(ip: string, write_api_key: string, n1: number, n2: number, n3: number, n4: number, n5: number, n6: number, n7: number, n8: number) {
+        if (wifi_connected && write_api_key != "") {
+            thingspeak_connected = false
+            sendAT("AT+CIPSTART=\"TCP\",\"" + ip + "\",80", 0)
+            thingspeak_connected = wait_for_response()
+            basic.pause(100)
+            if (thingspeak_connected) {
+                last_upload_successful = false
+                let str: string = "GET /update?api_key=" + write_api_key + "&field1=" + n1 + "&field2=" + n2 + "&field3=" + n3 + "&field4=" + n4 + "&field5=" + n5 + "&field6=" + n6 + "&field7=" + n7 + "&field8=" + n8
+                sendAT("AT+CIPSEND=" + (str.length + 2))
+                sendAT(str, 0)
+                last_upload_successful = wait_for_response()
+                basic.pause(100)
+            }
+        }
+    }
+
+    //% block="Wait %delay ms"
+    //% delay.min=0 delay.defl=5000
+    export function wait(delay: number) {
+        if (delay > 0) basic.pause(delay)
+    }
+
+    //% block="Wifi connected ?"
+    export function is_wifi_connected() {
+        return wifi_connected
+    }
+
+    //% block="ThingSpeak connected ?"
+    export function is_ThingSpeak_connected() {
+        return thingspeak_connected
+    }
+
+    //% block="Last upload successful ?"
+    export function is_last_upload_successful() {
+        return last_upload_successful
+    }
+
+}
